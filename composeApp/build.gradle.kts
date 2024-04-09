75a83520ee5458eabe72d1f435cb1b3595ced719
Tanish Azad <tanishazad03@gmail.com>
2024-04-09 20:08:39 +0530

    deps upgrade

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinxSerialization)
     alias(libs.plugins.kmpNativeCoroutines)
 }
